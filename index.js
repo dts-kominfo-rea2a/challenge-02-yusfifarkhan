49bbd16af98cb1aa5d467b953804de0a4aded155
yusfifarkhan <93718536+yusfifarkhan@users.noreply.github.com>
2022-05-27 20:48:32 +0700

    Submit #2
    Submit kedua setelah menyesuaikan output array.

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function lakukanLooping(arrPegawai) {
   for (let i = 0; i < dataYangAkanDilooping.length; i++) {
     // hasilLooping.push(dataYangAkanDilooping[1])
     // console.log(dataYangAkanDilooping[i].namaDepan + ", " + dataYangAkanDilooping[i].namaBelakang)
-    hasilLooping.push(dataYangAkanDilooping[i].namaDepan + ", " + dataYangAkanDilooping[i].namaBelakang);    
+    hasilLooping.push(dataYangAkanDilooping[i].namaDepan + " " + dataYangAkanDilooping[i].namaBelakang);    
   }
 
   /*
