d470836ff8ed21b21153784790638fc969605ca5
yusfifarkhan <93718536+yusfifarkhan@users.noreply.github.com>
2022-05-27 20:45:46 +0700

    Submit #1
    Submit pertama challenge 2

--- index.js
+++ index.js
@@ -23,19 +23,39 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  
+     
+  let hasilLooping = [];
+
+  for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+    // hasilLooping.push(dataYangAkanDilooping[1])
+    // console.log(dataYangAkanDilooping[i].namaDepan + ", " + dataYangAkanDilooping[i].namaBelakang)
+    hasilLooping.push(dataYangAkanDilooping[i].namaDepan + ", " + dataYangAkanDilooping[i].namaBelakang);    
+  }
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+  let jumlahPria = 0;
+
+  for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+    if(dataYangAkanDilooping[i].jenisKelamin === "M"){
+      jumlahPria += 1;
+    }
+  };
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
+  let jumlahWanita = 0;
+
+  for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+    if (dataYangAkanDilooping[i].jenisKelamin === "F") {
+      jumlahWanita += 1;
+    }
+  };
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -49,6 +69,16 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+
+  if(jumlahPria>jumlahWanita){
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  }else if (jumlahWanita > jumlahPria) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria"
+  }else if (jumlahPria === jumlahWanita) {
+    komentar = "Jumlah Pria dan Wanita berimbang"
+  } else {
+    komentar = komentar
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
